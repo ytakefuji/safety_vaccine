66bbe27fbde6518bb2fa1cc107e12f28e92d6e9e
Yoshiyasu Takefuji <ytakefuji@gmail.com>
2021-11-21 11:38:54 +0900

    Add files via upload

--- vaers.py
+++ vaers.py
@@ -12,17 +12,36 @@ NOVIDs=vax.loc[vax.VAX_MANU=="NOVARTIS VACCINES AND DIAGNOSTICS",'VAERS_ID']
 print("NOVIDs instances: ",len(NOVIDs))
 deathNOV=set(IDs).intersection(NOVIDs)
 print("NOVIDs deaths: ",len(deathNOV))
-print("NOV death per instance",round(len(deathNOV)/len(NOVIDs),3))
+print("NOV death per instance",round(len(deathNOV)/len(NOVIDs),6))
 
 MODERNAIDs=vax.loc[vax.VAX_MANU=="MODERNA",'VAERS_ID']
 print("MODERNAIDs instances: ",len(MODERNAIDs))
 deathMODERNA=set(IDs).intersection(MODERNAIDs)
 print("MODERNA deaths",len(deathMODERNA))
-print("MODERNA",round(len(deathMODERNA)/len(MODERNAIDs),3))
+print("MODERNA",round(len(deathMODERNA)/len(MODERNAIDs),6))
 
 PFIZERIDs=vax.loc[vax.VAX_MANU=="PFIZER\BIONTECH",'VAERS_ID']
 print("PFIZERIDs instances: ",len(PFIZERIDs))
 deathPFIZER=set(IDs).intersection(PFIZERIDs)
 print("PFIZER deaths",len(deathPFIZER))
-print("PFIZER",round(len(deathPFIZER)/len(PFIZERIDs),3))
+print("PFIZER",round(len(deathPFIZER)/len(PFIZERIDs),6))
+
+M_P=set(MODERNAIDs).intersection(PFIZERIDs)
+M_Pdeath=set(deathMODERNA).intersection(deathPFIZER)
+print('MODERNA+PFIZER:',len(M_P))
+print('MODERNA+PFIZER death:',len(M_Pdeath))
+print('MODERNA+PFIZER death per instance:',round(len(M_Pdeath)/len(M_P),6))
+
+MODERNAIDs=set(MODERNAIDs).difference(M_P)
+print("MODERNAIDs instances: ",len(MODERNAIDs))
+deathMODERNA=set(IDs).intersection(MODERNAIDs)
+print("MODERNA deaths",len(deathMODERNA))
+print("MODERNA",round(len(deathMODERNA)/len(MODERNAIDs),6))
+
+PFIZERIDs=set(PFIZERIDs).difference(M_P)
+print("PFIZERIDs instances: ",len(PFIZERIDs))
+deathPFIZER=set(IDs).intersection(PFIZERIDs)
+print("PFIZER deaths",len(deathPFIZER))
+print("PFIZER",round(len(deathPFIZER)/len(PFIZERIDs),6))
+
 
